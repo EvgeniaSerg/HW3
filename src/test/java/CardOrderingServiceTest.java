242cadf846a62d32b767f5ed201ad35fb4d651bc
Eugenia <st_shelkunova@list.ru>
2024-01-23 21:40:02 +0300

    the seventh commit

--- src/test/java/CardOrderingServiceTest.java
+++ src/test/java/CardOrderingServiceTest.java
@@ -9,7 +9,7 @@ public class CardOrderingServiceTest {
     @Test
     void shouldTestData() throws InterruptedException {
         open("http://localhost:9999/");
-        SelenideElement form = $("[method=post]");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Прохорова Анна-Мария");
         form.$("[data-test-id=phone] input").setValue("+79162354864");
         form.$("[data-test-id=agreement]").click();
